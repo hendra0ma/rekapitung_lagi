be1ef62a86b3389a7dc8d58eaf82f4c002d1fa44
isa-ardi <54489663+isa-ardi@users.noreply.github.com>
2022-12-29 13:28:01 +0700

    publick

--- resources/views/publik/kecamatan.blade.php
+++ resources/views/publik/kecamatan.blade.php
@@ -150,8 +150,40 @@ $data_masuk = (int)$saksidatai / (int)$dpt * 100;
                 </div>
 
                 <div class="row mt-5">
+                    <?php $i = 1; ?>
+                    @foreach ($paslon as $pas)
+                    <div class="col-lg col-md col-sm col-xl mb-3    ">
+                        <div class="card overflow-hidden" style="margin-bottom: 0px;">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col col-auto">
+                                        <div class="counter-icon box-shadow-secondary brround ms-auto candidate-name text-white" style="margin-bottom: 0; background-color: {{$pas->color}};">
+                                            {{$i++}}
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <h6 class="">{{$pas->candidate}} </h6>
+                                        <h6 class="">{{$pas->deputy_candidate}} </h6>
+                                        <?php
+                                        $voice = 0;
+                                        ?>
+                                        @foreach ($pas->saksi_data as $dataTps)
+                                        <?php
+                                        $voice += $dataTps->voice;
+                                        ?>
+                                        @endforeach <br>
+                                        <h3 class="mb-2 number-font">{{ $voice }} suara</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+
+                <div class="row mt-5">
                     <div class="col-md-12 mt-3">
-                        <h5 class="text-uppercase">HITUNG SUARA PEMILIHAN WALIKOTA DAN WAKIL WALIKOTA
+                        <h5 class="text-uppercase">HITUNG SUARA PEMILIHAN PILPRES DAN WAKIL PILPRES
                             {{$kota['name']}}
                         </h5>
                     </div>
@@ -162,36 +194,45 @@ $data_masuk = (int)$saksidatai / (int)$dpt * 100;
                     </div>
                 </div>
 
-
                 <div class="row mt-5">
                     <div class="col-md-12 mt-5">
-                        <div class="table-responsive">
-                            <table class="table table-bordered text-nowrap border-bottom">
-                                <thead class="bg-primary">
-                                    <tr>
-                                        <th class="text-white align-middle">Kecamatan</th>
-                                        <th class="text-white align-middle">Sumber Kelurahan</th>
-                                        <th class="text-white align-middle">Total TPS</th>
-                                        <th class="text-white align-middle">Quick Count</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($villages_quick as $item)
-                                    <?php $count_tps = Tps::where('villages_id',(string)$item['id'])->count(); ?>
-                                    <?php $count_tps_quick = Tps::where('villages_id',(string)$item['id'])->where('sample', 1)->count(); ?>
-                                    <tr>
-                                        <td class="align-middle">{{$item['name']}}</td>
-                                        <td class="align-middle">
-                                            <a href="modaltpsQuick" class="modaltpsQuick text-dark" style="font-size: 0.8em;" id="Cek" data-id="{{$item['id']}}" data-bs-toggle="modal" id="" data-bs-target="#modaltpsQuick">{{$item['name']}}</a>
-                                        </td>
-                                        <td class="align-middle">{{$count_tps}}</td>
-                                        <td class="align-middle">{{$count_tps_quick}}</td>
-                                    </tr>
+                        <table class="table table-bordered table-hover">
+                            <thead class="bg-primary">
+                                <tr>
+                                    <th class="align-middle text-white">kelurahan</th>
+                                    @foreach ($paslon_candidate as $item)
+                                    <th class="text-white">{{ $item['candidate']}}</th>
                                     @endforeach
 
-                                </tbody>
-                            </table>
-                        </div>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                @foreach ($kel as $item)
+
+
+                                <tr onclick='check("{{Crypt::encrypt($item->id)}}")'>
+                                    <td><a class="text-dark" href="/public/kelurahan/{{Crypt::encrypt($item['id'])}}">{{$item['name']}}</a></td>
+                                    @foreach ($paslon_candidate as $cd)
+                                    <?php $saksi_dataa = SaksiData::join('saksi', 'saksi.id', '=', 'saksi_data.saksi_id')
+                                        ->where('paslon_id', $cd['id'])
+                                        ->where('saksi_data.village_id',(string)$item['id'])
+                                        ->where('verification', 1)
+                                        ->sum('voice'); ?>
+                                    <td>{{$saksi_dataa}}</td>
+                                    @endforeach
+
+
+                                </tr>
+                                @endforeach
+                            </tbody>
+
+                            <script>
+                                let check = function(id) {
+                                    window.location = `public/kelurahan/${id}`;
+                                }
+                            </script>
+                        </table>
                     </div>
                 </div>
 
