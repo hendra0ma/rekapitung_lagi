173d10755da852060b386b4a47e6acf4903b8b83
hendra0ma <hendra0maulidan@gmail.com>
2023-02-14 03:34:41 +0700

    sad

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1194,7 +1194,7 @@ class AdminController extends Controller
         $data['index_tsm']    = ModelsListkecurangan::get();
         $data['list_suara']  = Tps::join('saksi', 'saksi.tps_id', '=', 'tps.id')
             ->join('users', 'users.tps_id', '=', 'tps.id')
-            ->whereNull("makamah_konsitusi")
+            ->whereNull("saksi.makamah_konsitusi")
             ->where('saksi.kecurangan', 'yes')
             ->where('saksi.status_kecurangan', 'terverifikasi')
             ->select('saksi.*', 'saksi.created_at as date', 'tps.*', 'users.*')
